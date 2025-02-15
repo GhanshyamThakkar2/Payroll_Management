cd3cea6e50bb04fe662cd44b1585d973285b489e
GhanshyamThakkar2 <23ceued008@ddu.ac.in>
2025-02-16 00:16:46 +0530

    UPDATE
    WITH DESIGN

--- Employee Management/obj/Debug/net8.0/Employee Management.AssemblyInfo.cs
+++ Employee Management/obj/Debug/net8.0/Employee Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebefa23dcffdc33a70d0f719b5b16280803865ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b48d95bff9710d37d55710e41402625640aa01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

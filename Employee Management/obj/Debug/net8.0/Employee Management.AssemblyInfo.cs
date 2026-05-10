f689d600940c23c455187137dbc8574aa3226d3f
GhanshyamThakkar123 <ghanshyamthakkar1123@gmail.com>
2026-05-10 23:46:44 +0530

    Done the proper design of the Details page

--- Employee Management/obj/Debug/net8.0/Employee Management.AssemblyInfo.cs
+++ Employee Management/obj/Debug/net8.0/Employee Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8047d51ba53049ee8f463a030e16c37191f8e42e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0554b998264bc07e99084049f1e2be33a3a506b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
